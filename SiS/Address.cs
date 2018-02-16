73903241a0c0e937d7cb5c451388e641da4176a7
JustinHuntGC <justin.hunt@georgiancollege.ca>
2018-02-15 19:32:00 -0500

    repush

---
+++ SiS/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiS
+{
+    public enum AddressTypes { Mailing, Home, }
+    public class Address
+    {
+        public String Line1 { get; set; }
+        public String Line2 { get; set; }
+        public String City { get; set; }
+        public String Province { get; set; }
+        public String Country { get; set; }
+        //public String Line1 { get; set; }
+
+    }
+}

5f44a04125dcc9468293d1c47a87b9bb02485cd8
JustinHuntGC <justin.hunt@georgiancollege.ca>
2018-03-01 17:23:48 -0500

    fileio project

---
+++ FileIO-L5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiS;
+namespace FileIO_L5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            for (int x = 0; x < 10; x++)
+            {
+                Student s = new Student("John", "Doe"+x, new DateTime(1980, 11, 01));
+                students.Add(s);
+            }
+            String header = "FirstName,LastName,DateOfBirth,ID";
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter("output.csv"))
+            {
+                file.WriteLine(header);
+                foreach (Student s in students)
+                {
+                    //https://github.com/justinhuntgc/COMP1098.3
+                    String line = s.FirstName.Replace(",","") + "," + s.LastName.Replace(",", "") + "," +
+                        s.DateOfBirth.ToShortDateString() + "," + s.ID.ID;
+                    file.WriteLine(line);
+                }
+            }
+
+
+        }
+    }
+}
